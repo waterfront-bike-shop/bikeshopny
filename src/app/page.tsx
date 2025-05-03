104424c7a3726b4e5fe2db57971c70e239b35e2c
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-05-03 17:49:05 -0400

    🚴‍♀️ Road Bike message replace 5 boro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,31 @@ import HomeCards from "./components/HomeCards";
 const HomePage: FC = () => {
   return (
     <>
+      {/* /////////////////////////////  */}
+      {/* DEFAULT Header                 */}
+      {/* /////////////////////////////  */}
+
       {/* <Hero headline="Welcome to the best Bike Shop on the Hudson River!" href="/rentals" button_text="Rent a bike online" image="/images/hudson_river_aerial_view.jpg"/> */}
+
+      {/* /////////////////////////////  */}
+      {/* In between                     */}
+      {/* /////////////////////////////  */}
+
       <Hero
+        headline="Road Bike Rentals available for Sunday, May 4, 2025:"
+        subheadings={[
+          "One BMC and Two Specialized Roubaix's Available!"
+        ]}
+        href="/rentals"
+        button_text="Rent a road bike"
+        image="/images/hudson_river_aerial_view.jpg"
+      />
+
+      {/* /////////////////////////////  */}
+      {/* Hero for the 5 Boro Bike Tour  */}
+      {/* /////////////////////////////  */}
+
+      {/* <Hero
         headline="Bikes still available to rent for the 2025 5 Boro Bike Tour"
         subheadings={[
           "$99 per bike for Sat May 03, and Sun May 04 two day minimum",
@@ -18,7 +41,7 @@ const HomePage: FC = () => {
         button_text="Book by May 3 at 5pm (Pickup by 6:45pm)"
         image="/images/5-boro-riders.jpg"
       />
-      <HomeCards />
+      <HomeCards /> */}
     </>
   );
 };
