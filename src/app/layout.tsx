26d995e1e3fb84147bba62343103146c5c2f1cc9
Nick Golebiewski <nick.golebiewski@gmail.com>
2025-05-05 21:41:43 -0400

    🚲 Fixed Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,45 +5,38 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Script from "next/script";
 import ClientAnalytics from "./components/ClientAnalytics"; // import ClientAnalytics component
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
 const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export const metadata: Metadata = {
-  title: 'Bicycle Rentals - Waterfront Bicycle Shop Bike Rental / Repair NYC',
+  title: "Bicycle Rentals - Waterfront Bicycle Shop Bike Rental / Repair NYC",
   description:
-    'We offer on-the-spot repairs and in-depth tune-ups for your bike. Rent hybrid bikes, shop parts, and ride along the NYC waterfront.',
+    "We offer on-the-spot repairs and in-depth tune-ups for your bike. Rent hybrid bikes, shop parts, and ride along the NYC waterfront.",
   openGraph: {
-    url: 'https://bikeshopny.com/',
-    siteName: 'Waterfront Bicycle Shop Bike Rental / Repair NYC',
-    title: 'Waterfront Bicycle Shop Bike Rental / Repair NYC',
+    url: "https://bikeshopny.com/",
+    siteName: "Waterfront Bicycle Shop Bike Rental / Repair NYC",
+    title: "Waterfront Bicycle Shop Bike Rental / Repair NYC",
     description:
-      'We offer on-the-spot repairs and in-depth tune-ups for your bike. Rent hybrid bikes, shop parts, and ride along the NYC waterfront.',
-    type: 'website',
+      "We offer on-the-spot repairs and in-depth tune-ups for your bike. Rent hybrid bikes, shop parts, and ride along the NYC waterfront.",
+    type: "website",
     images: [
       {
-        url: 'https://img1.wsimg.com/isteam/ip/5908d7da-7b85-40f0-a477-544892880bd5/47fb6dcb-0116-4cbc-8be8-9fa8a383e835.jpg',
+        url: "https://img1.wsimg.com/isteam/ip/5908d7da-7b85-40f0-a477-544892880bd5/47fb6dcb-0116-4cbc-8be8-9fa8a383e835.jpg",
       },
     ],
-    locale: 'en_US',
+    locale: "en_US",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'Waterfront Bicycle Shop Bike Rental / Repair NYC',
+    card: "summary_large_image",
+    title: "Waterfront Bicycle Shop Bike Rental / Repair NYC",
     description:
-      'We offer on-the-spot repairs and in-depth tune-ups for your bike. Rent hybrid bikes, shop parts, and ride along the NYC waterfront.',
+      "We offer on-the-spot repairs and in-depth tune-ups for your bike. Rent hybrid bikes, shop parts, and ride along the NYC waterfront.",
     images: [
-      'https://img1.wsimg.com/isteam/ip/5908d7da-7b85-40f0-a477-544892880bd5/47fb6dcb-0116-4cbc-8be8-9fa8a383e835.jpg',
+      "https://img1.wsimg.com/isteam/ip/5908d7da-7b85-40f0-a477-544892880bd5/47fb6dcb-0116-4cbc-8be8-9fa8a383e835.jpg",
     ],
-    creator: '@bikeshopny',
+    creator: "@bikeshopny",
   },
-  icons: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      url: '/logos/waterfront-bicycle-shop-logo.png',
-    },
-  ],
 };
 
 export default function RootLayout({
@@ -54,6 +47,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" />
         {GA_ID && (
           <>
             <Script
@@ -75,7 +69,8 @@ export default function RootLayout({
       </head>
       <body>
         <Header />
-        <ClientAnalytics /> {/* ClientAnalytics component to track route changes */}
+        <ClientAnalytics />{" "}
+        {/* ClientAnalytics component to track route changes */}
         {children}
         <Footer />
       </body>
